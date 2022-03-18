caa52175129d9ec6703d4fb8bc96621c87df4b03
MCJBYDAY <jacobsmichaelc@gmail.com>
2022-03-18 14:33:59 -0500

    added some more familial colors and button hover states

--- etch.js
+++ etch.js
@@ -17,7 +17,7 @@ function makeGrid(rows, cols) {
   // css handles positioning 
   for (var cellCount = 1; cellCount <= (rows * cols); cellCount++) {
     let cell = document.createElement("div");
-    cell.innerText = cellCount;
+    cell.innerText = 0;
     // container.appendChild(cell).className = "grid_element"; 
     cell.classList.add("grid_element");
     container.appendChild(cell);
@@ -47,43 +47,3 @@ function startPage() {
 
 startPage();
   
-
-
-//  not appropriate .. constructor approach some eloquent javascript -- seeking to each element an object
-//  function Grid(width, height) {
-//     this.space = new Array(width * height);
-//     this.width = width;
-//     this.height = height;
-//   }
-//   Grid.prototype.isInside = function(vector) {
-//     return vector.x >= 0 && vector.x < this.width &&
-//            vector.y >= 0 && vector.y < this.height;
-//   };
-//   Grid.prototype.get = function(vector) {
-//     return this.space[vector.x + this.width * vector.y];
-//   };
-//   Grid.prototype.set = function(vector, value) {
-//     this.space[vector.x + this.width * vector.y] = value;
-//   };
-  
-// not appropriate .. nested for loop isn't appropriate 
-// given the fact that the display: grid property already sets columns and rows out of box
-//
-// function makeGrid(rows, cols) {
-//     container.style.setProperty('--grid-rows', rows);
-//     container.style.setProperty('--grid-cols', cols);
-    
-//     for (r = 0; r < rows; r++) {
-//         for (c = 0; c < cols; c++) {
-//             let cell = document.createElement("div");
-//             cell.innerText = (s + 1);
-//             container.appendChild(cell).className = "grid_element";
-//         }
-//     };
-//         let cell = document.createElement("div");
-//       cell.innerText = (s + 1);
-//       container.appendChild(cell).className = "grid_element";
-// };
-
-
-//   makeGrid(4, 4);
