a4ff8890ecd1657e2f6d277d6e81cad304d74136
MCJBYDAY <jacobsmichaelc@gmail.com>
2022-03-23 17:19:52 -0500

    sorted out code to be inclusive of basic grid layout along with a normal and dark mode enabled by js functions

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Big Welcome
-Welcome to digizen, a place that records a 2 dimensional record of where you hovered your mouse.
+Welcome to digizen, a place that records a 2 dimensional record of where you hovered your mouse. <br>
 # Why?
-We all can benefit from a bit of recorded mouse hovering from time to time.
+We all can benefit from a bit of recorded mouse hovering from time to time.<br>
 # Who commissioned this brilliance?
-Visit [The Odin Project](https://www.theodinproject.com/) to learn about a community that is helping developers understand programming concepts at no cost to them
+Visit [The Odin Project](https://www.theodinproject.com/) to learn about a community that is helping developers understand programming concepts at no cost to them.
 
--- etch.js
+++ etch.js
@@ -2,53 +2,136 @@
 digizen
 etch.js
 Michael Jacobs
-3/16/2022
+3/23/2022
 */
 
+const container = document.querySelector("#grid_container");
+const interface = document.querySelector("#interface");
+const buttonNorm = document.createElement("button");
+const buttonDark = document.createElement("button");
+const buttonClear = document.createElement("button");
+var lastMode = '';
 
-var container = document.getElementById("grid_container");
-var constraint = document.getElementById("grid_frame").clientWidth;
-console.log(constraint);
-
-function makeGrid(width) {
-  var blockdim = Math.round((constraint)/(width));
-  // container.style.setProperty('--grid-columns', cols);
-  // container.style.setProperty('--grid(width',(width);
-
-  // creates individual cells, iterating over a number of (width and columns
-  // css handles positioning 
-  for (var cellCount = 1; cellCount <= (width * width); cellCount++) {
+function makeGrid(rows, cols) {
+  for (var cellCount = 1; cellCount <= (rows * cols); cellCount++) {
     let cell = document.createElement("div");
-    // cell.innerText = cellCount;
-    cell.style.width = `${blockdim}px`;
-    // cell.style.setProperty('clientWidth', `${blockdim}px`);
-    cell.style.height =`${blockdim}px`;
-    // container.appendChild(cell).className = "grid_element"; 
+    cell.style.border = '1px solid #ebe198';
+    container.style.gridTemplateColumns = `repeat(${cols}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${rows}, 1fr)`
     cell.classList.add("grid_element");
     container.appendChild(cell);
+    // alternative one line syntax
+    // container.appendChild(cell).className = "grid_element"; 
     };  
 }
 
-function startEtching() {
+function etchingNormal() {
   const cellItems = document.querySelectorAll('#grid_container > div');
- cellItems.forEach((item) => {
-    item.count = 0;
-    item.addEventListener('mouseenter', (e) => {
-      e.target.style.backgroundColor = '#6e6e6e';
-      e.target.style.opacity = 1;
-      // e.target.backgroundColor = white;
-      });
+  const mainBackG = document.querySelector('.main_container');
+  const containerBackG = document.querySelector('.interactive_container');
+  buttonNorm.textContent = "Normal Mode";
+  buttonNorm.addEventListener('click' , () => {
+    mainBackG.style.backgroundColor = '#FFFFFF';
+    containerBackG.style.backgroundColor = '#939597';
+    cellItems.forEach((item) => {
+      item.style.backgroundColor = '#b6b18f';
+      item.count = 0;
+      item.addEventListener('mouseenter', (e) => {
+        e.target.style.backgroundColor = '#5c6470';
+        e.target.style.opacity = 1;
+        });
+      })
   });
+  lastMode = '#b6b18f';
+  interface.appendChild(buttonNorm).classList.add('btn');
 }
 
+function etchingDark() {
+  const cellItems = document.querySelectorAll('#grid_container > div');
+  const mainBackG = document.querySelector('.main_container');
+  const containerBackG = document.querySelector('.interactive_container');
+  buttonDark.textContent = "Dark Mode";
+  buttonDark.addEventListener('click' , () => {
+    mainBackG.style.backgroundColor = '#000000';
+    containerBackG.style.backgroundColor = '#252525';
+    cellItems.forEach((item) => {
+      item.count = 0;
+      item.style.backgroundColor = '#B9B9B9';
+      item.addEventListener('mouseenter', (e) => {
+        e.target.style.backgroundColor = '#252525';
+        e.target.style.opacity = 1;
+        });
+      })
+  });
+  lastMode = '#B9B9B9';
+  interface.appendChild(buttonDark).classList.add('btn');
+}
+
+// resetEtched and etchClear are removed until I can sort out saving the prior mode setting
+// function resetEtched() {
+//   const cellItems = document.querySelectorAll('#grid_container > div');
+//   cellItems.forEach((item) => {
+//     item.style.backgroundColor = lastMode;
+//   });
+// }
+
+// function etchClear() {
+//   buttonClear.textContent = "Clear Etching";
+//   buttonClear.addEventListener('click' , () => {
+//     resetEtched();
+//   });
+//   interface.appendChild(buttonClear).classList.add('btn');
+// }
 
 function startPage() {
-  makeGrid(16);
-  startEtching();
+  makeGrid(16, 16);
+  etchingNormal();
+  etchingDark();
+  // etchClear is removed until I can sort out saving the prior mode setting
+  // etchClear();
   //changeSize();
   //changeMode();
-  //eraseListener();
 }
 
 startPage();
   
+
+
+//  not appropriate .. constructor approach some eloquent javascript -- seeking to each element an object
+//  function Grid(width, height) {
+//     this.space = new Array(width * height);
+//     this.width = width;
+//     this.height = height;
+//   }
+//   Grid.prototype.isInside = function(vector) {
+//     return vector.x >= 0 && vector.x < this.width &&
+//            vector.y >= 0 && vector.y < this.height;
+//   };
+//   Grid.prototype.get = function(vector) {
+//     return this.space[vector.x + this.width * vector.y];
+//   };
+//   Grid.prototype.set = function(vector, value) {
+//     this.space[vector.x + this.width * vector.y] = value;
+//   };
+  
+// not appropriate .. nested for loop isn't appropriate 
+// given the fact that the display: grid property already sets columns and rows out of box
+//
+// function makeGrid(rows, cols) {
+//     container.style.setProperty('--grid-rows', rows);
+//     container.style.setProperty('--grid-cols', cols);
+    
+//     for (r = 0; r < rows; r++) {
+//         for (c = 0; c < cols; c++) {
+//             let cell = document.createElement("div");
+//             cell.innerText = (s + 1);
+//             container.appendChild(cell).className = "grid_element";
+//         }
+//     };
+//         let cell = document.createElement("div");
+//       cell.innerText = (s + 1);
+//       container.appendChild(cell).className = "grid_element";
+// };
+
+
+//   makeGrid(4, 4);
